d2b45c03e716e6391d3e308fdf128659581e53b8
SaurabhCybage <saurabhgupt@cybage.com>
2017-02-02 20:15:45 +0530

    1. build folder added in git ignore. 2. move.bat removed.

--- move.bat
+++
@@ -1,8 +0,0 @@
-REM your project resource folder's path
-set resourceFolder=C:\Users\saurabhgupt\Downloads\Android-SDK-master\VungleSalesDemoApp\app\src\main\res
-
-move .\drawable-hdpi\*.png %resourceFolder%\drawable-hdpi
-move .\drawable-mdpi\*.png %resourceFolder%\drawable-mdpi
-move .\drawable-xhdpi\*.png %resourceFolder%\drawable-xhdpi
-move .\drawable-xxhdpi\*.png %resourceFolder%\drawable-xxhdpi
-move .\drawable-xxxhdpi\*.png %resourceFolder%\drawable-xxxhdpi
